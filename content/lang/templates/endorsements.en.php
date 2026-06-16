0f6ed0149e40977f834a55220e70760207c76679
Lindsay Walker <lindsay.walker@ku.edu>
2026-06-16 16:27:41 -0700

    Add endorsement text
    Updated endorsement text to include new text and hyperlinks.

--- content/lang/templates/endorsements.en.php
+++ content/lang/templates/endorsements.en.php
@@ -7,8 +7,7 @@ Language: Espa�ol (Spanish)
 
 $LANG['HOME'] = 'Home';
 $LANG['ENDORSEMENTS'] = 'Endorsements';
-$LANG['ENDORSING_ORGS'] = 'The American Bryological and Lichenological Society (ABLS) officially endorses the Consortium of 
-Lichen Herbaria as a platform to share and manage biodiversity data on lichenized fungi. We invite collaborators and institutions 
-worldwide to consider participating by joining the Consortium.';
-
+$LANG['ENDORSING_ORGS'] = 'The <a href="https://www.abls.org/" target="_blank">American Bryological and Lichenological Society</a> (ABLS) officially endorses the Consortium of Lichen Herbaria as a platform to share and manage biodiversity data on lichenized fungi. We invite collaborators and institutions worldwide to consider participating by joining the Consortium.
+<br><br>
+The <a href="https://www.californialichens.org" target="_blank">California Lichen Society</a> (CALS) officially endorses the Consortium of Lichen Herbaria as a platform to share and manage biodiversity data on lichenized fungi. We invite collaborators and institutions worldwide to consider participating by joining the Consortium.';
 ?>
