b39eb52b52bb3f745b6c14c3d18421e9c0829f5c
Katie Pearson <katelin.d.pearson24@gmail.com>
2026-09-09 13:43:18 -0700

    Update endorsement

--- content/lang/templates/endorsements.en.php
+++ content/lang/templates/endorsements.en.php
@@ -9,11 +9,11 @@ $LANG['HOME'] = 'Home';
 $LANG['ENDORSEMENTS'] = 'Endorsements';
 $LANG['ENDORSING_ORGS'] = 'The <a href="https://ial-lichenology.org/" target="_blank">International Association for Lichenology</a> (IAL) recognizes the importance of open access to biodiversity data on lichenized, lichenicolous and allied fungi. The IAL therefore officially endorses the Consortium of Lichen Herbaria, which we encourage our members to join, contribute, and collaborate with.
 <br><br>
-The <a href="https://www.abls.org/" target="_blank">American Bryological and Lichenological Society</a> (ABLS) officially endorses the Consortium of Lichen Herbaria as a platform to share and manage biodiversity data on lichenized fungi. We invite collaborators and institutions worldwide to consider participating by joining the Consortium.
+The <a href="https://www.abls.org/" target="_blank">American Bryological and Lichenological Society</a> (ABLS) officially endorses the Consortium of Lichen Herbaria as a platform to share and manage biodiversity data on lichenized, lichenicolous and allied fungi. We invite collaborators and institutions worldwide to consider participating by joining the Consortium.
 <br><br>
-The <a href="https://www.californialichens.org" target="_blank">California Lichen Society</a> (CALS) officially endorses the Consortium of Lichen Herbaria as a platform to share and manage biodiversity data on lichenized fungi. We invite collaborators and institutions worldwide to consider participating by joining the Consortium.
+The <a href="https://www.californialichens.org" target="_blank">California Lichen Society</a> (CALS) officially endorses the Consortium of Lichen Herbaria as a platform to share and manage biodiversity data on lichenized, lichenicolous and allied  fungi. We invite collaborators and institutions worldwide to consider participating by joining the Consortium.
 <br><br>
-<a href="https://northwest-lichenologists.wildapricot.org/" target="_blank">Northwest Lichenologists</a> regularly use and depend on the Consortium of Lichen Herbaria as an official platform to share and manage biodiversity data on lichenized fungi. This data-sharing forum for specimen-based data is unique and indispensable for global and regional studies of biodiversity, 
+<a href="https://northwest-lichenologists.wildapricot.org/" target="_blank">Northwest Lichenologists</a> regularly use and depend on the Consortium of Lichen Herbaria as an official platform to share and manage biodiversity data on lichenized, lichenicolous and allied fungi. This data-sharing forum for specimen-based data is unique and indispensable for global and regional studies of biodiversity, 
 air quality and biological effects of environmental changes. Our members and our associated regional herbaria are deeply invested in the data stored in CLH.';
 
 ?>
--- content/lang/templates/endorsements.es.php
+++ content/lang/templates/endorsements.es.php
@@ -9,12 +9,12 @@ $LANG['HOME'] = 'Inicio';
 $LANG['ENDORSEMENTS'] = 'Recomendaciones';
 $LANG['ENDORSING_ORGS'] = 'La <a href="https://ial-lichenology.org/" target="_blank">Asociación Internacional de Liquenología</a> (IAL) reconoce la importancia del acceso abierto a los datos sobre la biodiversidad de hongos liquenizados, liquenícolas y afines. Por ello, la IAL respalda oficialmente al Consortium of Lichen Herbaria, e invita a sus miembros a unirse a él, así como a contribuir y colaborar con dicha iniciativa.
 <br><br>
-La <a href="https://www.abls.org/" target="_blank">Sociedad Americana de Briología y Liquenología</a> (ABLS) recomienda oficialmente al Consorcio de Herbarios de Líquenes como plataforma para compartir y gestionar datos de biodiversidad sobre hongos liquenizados. Invitamos a colaboradores e 
+La <a href="https://www.abls.org/" target="_blank">Sociedad Americana de Briología y Liquenología</a> (ABLS) recomienda oficialmente al Consorcio de Herbarios de Líquenes como plataforma para compartir y gestionar datos de biodiversidad sobre hongos liquenizados, liquenícolas y afines. Invitamos a colaboradores e 
 instituciones de todo el mundo a considerar su participación uniéndose al Consorcio.
 <br><br>
 La <a href="https://www.californialichens.org" target="_blank">Sociedad de Líquenes de California</a> (CALS) recomienda oficialmente al Consorcio de Herbarios 
-de Líquenes como plataforma para compartir y gestionar datos de biodiversidad sobre hongos liquenizados. Invitamos a colaboradores e instituciones de todo el mundo a considerar su participación uniéndose al Consorcio.
+de Líquenes como plataforma para compartir y gestionar datos de biodiversidad sobre hongos liquenizados, liquenícolas y afines. Invitamos a colaboradores e instituciones de todo el mundo a considerar su participación uniéndose al Consorcio.
 <br><br>
-<a href="https://northwest-lichenologists.wildapricot.org/" target="_blank">Northwest Lichenologists</a> utiliza y depende habitualmente del Consortium of Lichen Herbaria (CLH) como plataforma oficial para compartir y gestionar datos de biodiversidad sobre hongos liquenizados. Este foro de intercambio de datos basados ​​en especímenes es único e indispensable para estudios globales y regionales sobre biodiversidad,
+<a href="https://northwest-lichenologists.wildapricot.org/" target="_blank">Northwest Lichenologists</a> utiliza y depende habitualmente del Consortium of Lichen Herbaria (CLH) como plataforma oficial para compartir y gestionar datos de biodiversidad sobre hongos liquenizados, liquenícolas y afines. Este foro de intercambio de datos basados ​​en especímenes es único e indispensable para estudios globales y regionales sobre biodiversidad,
 calidad del aire y los efectos biológicos de los cambios ambientales. Nuestros miembros y los herbarios regionales asociados están profundamente comprometidos con los datos almacenados en el CLH.';
 ?>
